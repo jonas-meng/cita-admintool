30808cc97ff7e94b505695ec833dc869ac6b8448
Xuming Meng <jonas.meng.jm@gmail.com>
2018-03-10 08:55:56 +0100

    key address generation finished

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,69 @@
 extern crate secp256k1;
 extern crate rand;
+extern crate crypto;
+extern crate bitcoin;
 
-use std::env;
-use std::fs::create_dir_all;
-use std::fs::OpenOptions;
-use std::io;
-use std::io::prelude::*;
-use std::process;
+use std::ops::Index;
 use secp256k1::Secp256k1;
-use secp256k1::ContextFlag;
-use secp256k1::key;
+use bitcoin::util::hash;
+use crypto::sha3::Sha3;
+use crypto::digest::Digest;
 
-fn mk_keys_addr() {
-    /*
-    let args: Vec<String> = env::args().collect();
-    let path = if args.len() == 2 {
-        format!("{}",args[1])
-    } else {
-        format!("{}/node{}",args[1],args[2])
-    };
-
-    if let Err(e) = create_dir_all(&path) {
-        println!("{}",e);
-        process::exit(1);
-    }
-
-    let dump_path = format!("{}/privkey",path);
-    let auth_path = format!("{}/authorities",args[1]);
-    */
-    
-    let full = Secp256k1::with_caps(ContextFlag::Full);
-    let (sk, pk) = full.generate_keypair(&mut rand::thread_rng()).unwrap();
-    println!("{}",to_hex_string(&sk));
-
-    // Notice: dump file shouldn't exist
-    /*
-    if let Err(e) = write_to_file(&dump_path,"This is dump path") {
-        println!("{}",e);
-        process::exit(1);
-    }
-
-    if let Err(e) = write_to_file(&auth_path,"This is dump path") {
-        println!("{}",e);
-        process::exit(1);
-    }
-    */
-}
-
-pub fn to_hex_string(sk: &key::SecretKey) -> String {
-    let &key::SecretKey(bytes) = sk;
-    let strs: Vec<String> = bytes.iter().map(|b| format!("{:02X}", b)).collect();
+pub fn to_hex(bytes: &[u8]) -> String {
+    println!("{}", bytes.len());
+    let strs: Vec<String> = bytes.iter()
+        .map(|b| format!("{:02X}", b))
+        .collect();
     strs.join("")
 }
 
-fn write_to_file(file_path: &str, message: &str) -> Result<(), io::Error> {
-    // try to create a file if not exist
-    // append message to the end of the file
-    let mut f = try!(OpenOptions::new()
-            .append(true)
-            .create(true)
-            .open(file_path));
-    try!(f.write_all(message.as_bytes()));
-    Ok(())
+pub fn generate_key_addr() -> (String, String) {
+    /*
+     * privkey: 32-byte
+     * pubkey: 64-byte
+     * address: 20-byte, no extended
+     *          24-byte, extedned
+     *  keccak256(pubkey)[12..], if no extended
+     *  keccak256(pubkey)[12..] + checksum, extended
+     * */
+    let the_secp256k1 = Secp256k1::new();
+    let (privkey, pubkey) = the_secp256k1.generate_keypair(&mut rand::thread_rng()).unwrap();
+    // private key 32-byte
+    let sk = &privkey.index(..);
+    // publick key 64-byte
+    let pk = &pubkey.serialize_vec(&the_secp256k1, false)[1..65];
+    let sk_hex = to_hex(sk);
+    println!("PRIVATE KEY: {}", sk_hex);
+    println!("PUBLIC KEY: {}", to_hex(pk));
 
+    let mut keccak = Sha3::keccak256();
+    let mut pubhash = [0; 32];
+    keccak.input(pk);
+    keccak.result(&mut pubhash);
+    // public hash 20-byte
+    let ph = &pubhash[12..];
+    println!("PUBLIC HASH: {}", to_hex(ph));
+
+    let extended = true;
+    let addr_hex: String;
+    if extended {
+        let mut checksum = [0; 32];
+        keccak.reset();
+        keccak.input(ph);
+        keccak.result(&mut checksum);
+        let cksm = &checksum[0..4];
+        println!("CHECKSUM: {}", to_hex(cksm));
+        let mut address: Vec<u8> = Vec::with_capacity(24);
+        address.extend_from_slice(ph);
+        address.extend_from_slice(cksm);
+        addr_hex = format!("0x{}", to_hex(&address));
+    } else {
+        addr_hex = format!("0x{}",to_hex(ph));
+    }
+    println!("ADDRESS: {}", addr_hex);
+    (sk_hex, addr_hex)
 }
 
 fn main() {
-    mk_keys_addr();
+    generate_key_addr();
 }
